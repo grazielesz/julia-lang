04ded3e647e7582a21bc4dd704fdd4c3fbb0eb33
Graziele Lopes de Souza <93684576+grazielesz@users.noreply.github.com>
2022-02-05 10:13:19 -0300

    upload
    teste em jl

---
+++ teste.jl
@@ -0,0 +1,9 @@
+println("o meu primeiro teste em julia! :)")
+println("=================================")
+nome_variavel = 78
+
+if nome_variavel == 78
+    print("o número é igual a 78.")
+else
+    print("o número não é 78. tente novamente. :(")
+end
